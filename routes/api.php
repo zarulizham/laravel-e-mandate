8422b03b0ef66dead33ae8ad3d3a19e0fe6132ca
Zarul Zubir <zarul.izham@gmail.com>
2022-11-16 20:16:41 +0800

    Fix routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,3 @@
 <?php
 
-use JagdishJP\FpxPayment\Fpx;
 use Illuminate\Support\Facades\Route;
-use JagdishJP\FpxPayment\Http\Controllers\PaymentController;
-
-// Route::get('fpx/transaction/status/{reference_id?}', function ($reference_id = '') {
-
-// 	$response = Fpx::getTransactionStatus($reference_id);
-// 	return $response;
-// })->name('fpx.transaction.status');
-
-
-// Route::get('banks', [PaymentController::class, 'banks'])->name('api.banks.index');
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Config;
 use App\Http\Controllers\FPX\Controller;
-use Monarobase\CountryList\CountryListFacade;
 use ZarulIzham\EMandate\Http\Controllers\PaymentController;
 
 $directPath = Config::get('e-mandate.direct_path');
@@ -11,19 +10,19 @@ $indirectPath = Config::get('e-mandate.indirect_path');
 
 Route::post('e-mandate/payment/auth', [PaymentController::class, 'handle'])->name('e-mandate.payment.auth.request');
 
-Route::post($directPath, [Controller::class, 'webhook'])->name('fpx.payment.direct.callback');
-Route::post($indirectPath, [Controller::class, 'callback'])->name('fpx.payment.indirect.callback');
+Route::post($directPath, [Controller::class, 'webhook'])->name('e-mandate.payment.direct.callback');
+Route::post($indirectPath, [Controller::class, 'callback'])->name('e-mandate.payment.indirect.callback');
 
-Route::match(
-    ['get', 'post'],
-    'fpx/initiate/payment/{iniated_from?}/{test?}',
-    [Controller::class, 'initiatePayment']
-)->name('fpx.initiate.payment');
+// Route::match(
+//     ['get', 'post'],
+//     'fpx/initiate/payment/{iniated_from?}/{test?}',
+//     [Controller::class, 'initiatePayment']
+// )->name('fpx.initiate.payment');
 
-Route::get(
-    'fpx/csr/request',
-    function () {
-        $countries = CountryListFacade::getList('en');
-        return view('fpx-payment::csr_request', compact('countries'));
-    }
-)->name('fpx.csr.request');
+// Route::get(
+//     'fpx/csr/request',
+//     function () {
+//         $countries = CountryListFacade::getList('en');
+//         return view('fpx-payment::csr_request', compact('countries'));
+//     }
+// )->name('fpx.csr.request');
